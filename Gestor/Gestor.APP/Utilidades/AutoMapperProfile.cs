289b1dd26c40be7c358dca8a178e76ac0778077d
joskeiner urdaneta <joskeiner.simosa9@gmail.com>
2023-11-28 16:39:42 -0300

    implementacion de mapper

---
+++ Gestor/Gestor.APP/Utilidades/AutoMapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Gestor.APP.Models.ViewModels;
+using Gestor.ENTITY.Models;
+
+namespace Gestor.APP.Utilidades
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() 
+        {
+            #region Categoria
+            CreateMap<Categoria, VMCategoria>()
+                .ForMember(d => d.EsActivo,
+                                    p => p.MapFrom(o => o.EsActivo == true ? 1 : 0));
+            CreateMap<VMCategoria, Categoria>()
+                .ForMember(d => d.EsActivo,
+                                    p => p.MapFrom(o => o.EsActivo == 1 ? true : false));
+
+            #endregion
+
+        }
+    }
+}
---
+++ Gestor/Gestor.APP/Utilidades/GenericResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Gestor.APP.Utilidades
+{
+    public class GenericResponse<TObject>
+    {
+        public bool Estado { get; set; }
+
+        public string? Mensaje { get; set; } 
+
+        public TObject? objeto { get; set; }    
+
+        public List<TObject> ListaObjeto { get; set; }
+    }
+}
