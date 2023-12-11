7cd8a91c42a05022a1c79b4115419db8e3b03317
joskeiner urdaneta <joskeiner.simosa9@gmail.com>
2023-12-11 15:10:06 -0300

    configuracion de  vista Producto con jQuery

--- Gestor/Gestor.APP/wwwroot/js/vistas/Producto_Index.js
+++ Gestor/Gestor.APP/wwwroot/js/vistas/Producto_Index.js
@@ -39,3 +39,80 @@ function mostrarModal(modelo = MODELO_BASE) {
 $("#btnNuevo").click(function () {
     mostrarModal();
 })
+
+
+//----- Lista de productos ---------
+
+$(document).ready(function () {
+
+    fetch("/Categoria/Lista")
+
+        .then(response => {
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+
+        .then(responseJson => {
+            if (responseJson.data.length > 0) {
+                responseJson.data.forEach((item) => {
+                    $("#cboCategoria").append($("<option>").val(item.CategoriaId).text(item.descripcion))
+                })
+            }
+        })
+
+    tabalaData = $("#tbdata").DataTable({
+        responsive: true,
+
+        "ajax": {
+            "url": "/Producto/Lista",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "Id", "visible": false, "searchable": false },
+            {
+                "data": "urlImagen", render: function (data) {
+                    return `<img style="height:60px" src=${data}/ class="rounded mx-auto d-block"> `
+                }
+            },
+            { "data": "codigoBarra" },
+            { "data": "marca" },
+            { "data": "descripcion" },
+            { "data": "nombreCategoria" },
+            { "data": "stock" },
+            { "data": "precio" },
+            {
+                "data": "esActivo", render: function (data) {
+                    if (data == 1) {
+                        return '<span class = "badge badge-info">Activo</span>';
+                    } else {
+                        return '<span class = "badge badge-danger">No Activo</span>';
+                    }
+                }
+            },
+            {
+                "dafaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class"fas fa-pancil-alt"></i></button>' +
+                    '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class"fas fa-pancil-alt"></i></button>',
+                "orderable": false,
+                "searchable": false,
+                "width": "90px"
+            }
+        ],
+        order: [[0, "desc"]],
+        dom: "Bfrtrip",
+        buttons: [
+            {
+                text: 'Exportar Execel',
+                extend: 'execlHtml5',
+                title: '',
+                filename: 'reporte Productos',
+                exportOptions: {
+                    columns: [2, 3, 4, 5, 6, 7]
+                }
+            }, 'pageLength'
+        ],
+        languaje: {
+
+            url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
+        },
+    });
+});
